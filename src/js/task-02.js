99c3f6b1b5fba934496b7ab375ac4f2ea522dc5d
BondAndrii <CitrusOk777@gmail.com>
2024-12-24 17:04:40 +0200

    done task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// find node-element to inject items
+const list = document.querySelector("#ingredients");
+
+//create function who will add new element to list
+const addNewElement = (ing) => {
+
+  // create one item
+  const item = document.createElement("li");
+  item.classList.add('item')
+  item.textContent = ing;
+  return item;
+ 
+}
+//create array of items
+const arrayItems = ingredients.map((ingredient) => addNewElement(ingredient));
+
+// add new items to list
+list.append(...arrayItems);
+
+
+
+
