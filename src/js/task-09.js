1e3e3b598aaf4862593af6e82c8c6d89dd35c344
BondAndrii <CitrusOk777@gmail.com>
2024-12-31 18:40:43 +0200

    changing background color by button

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+
+const changeBtn = document.querySelector(".change-color");
+
+const colorCode = bodyRef.querySelector(".color");
+
+const onClick = () => {
+  const color = getRandomHexColor();
+  bodyRef.style.backgroundColor = color;
+  colorCode.textContent = color;
+
+}
+
+changeBtn.addEventListener("click", onClick);
