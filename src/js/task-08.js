7a1f0cedfa437b955c66c7ff912577e66f103fd7
BondAndrii <CitrusOk777@gmail.com>
2024-12-31 18:10:43 +0200

    geting form-data

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,29 @@
 
+const registerFormRef = document.querySelector(".login-form ");
+
+const onFormSubmit = event => {
+    event.preventDefault();
+
+    const { currentTarget } = event;
+    const { elements } = currentTarget;
+    const { email, password } = elements;
+
+    const userData = {
+                email: email.value,
+                password: password.value
+            }   
+    const log = () => {
+        console.log(userData);
+        event.currentTarget.reset();
+    }
+
+    userData.email === '' || userData.password === '' ?
+        alert("Всі поля повинні бути заповнені") : log();
+
+    
+    
+}
+
+registerFormRef.addEventListener('submit', onFormSubmit);
+
+
